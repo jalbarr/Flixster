ff35fc6454ec2cfc385585cbf5ceefdb5dbd8ac5
Job Albarr <Albarr@gmx.com>
2019-02-12 19:06:08 -0500

    Flixter
    Flixter movie preview app

---
+++ flixster/MoviesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MoviesViewController.swift
+//  flixster
+//
+//  Created by Job Albarr on 2/6/19.
+//  Copyright © 2019 Job Albarr. All rights reserved.
+//
+
+import UIKit
+
+class MoviesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
