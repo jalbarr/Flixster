808f2cb93b5fc557b57637d4c73bd2a3ea0560b3
Job Albarr <Albarr@gmx.com>
2019-03-06 11:12:57 -0500

    Final Steps
    Finished Collection view layout

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Job Albarr on 2/28/19.
+//  Copyright © 2019 Job Albarr. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
